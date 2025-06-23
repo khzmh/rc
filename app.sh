3f13b49c391f3267d02c5c3c27e342786560db34
khzmh <sitikhojaimah@gmail.com>
2025-06-24 00:05:31 +0800

    Delete app.sh

--- app.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export OS_AUTH_TYPE=v3applicationcredential
-export OS_AUTH_URL=https://identity.brno.openstack.cloud.e-infra.cz/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_REGION_NAME="Brno1"
-export OS_INTERFACE=public
-export OS_APPLICATION_CREDENTIAL_ID=ee9a513134bc4b44b8dcc4d343144266
-export OS_APPLICATION_CREDENTIAL_SECRET=_5g0HPExSVfBpLKR3liTFyt8Wugkn7qFwF_9hIb-apMtV9gMh2IpVRORfFZ9R0CK-oaOtcDly90DlfFvYpaB2w
